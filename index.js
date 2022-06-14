9acfd981caea1a01144794d089e89d2120c7a1ae
Aigul <sharipova.aig@gmail.com>
2022-06-13 20:46:28 -0400

    js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const createTodo = document.querySelector('#todoContainer');
+const buttonAdd = document.querySelector('.btn');
+const todoContainer = document.querySelector('.todoContainer');
+const inputField = document.querySelector('.inputField');
+
+buttonAdd.addEventListener('click', function(){
+const item = document.createElement('li');
+})
