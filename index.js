970997d4fba4c7b27b5b27379aa993359b420f9b
Aigul <sharipova.aig@gmail.com>
2022-06-13 22:16:50 -0400

    style input

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const toDoContainer = document.querySelector('.toDoContainer');
 btn.addEventListener('click', function(){
 const item = document.createElement('li');
 item.innerText = inputField.value;
+item.classList.add('toDoAdded');
 toDoContainer.appendChild(item);
 })
