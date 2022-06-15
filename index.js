15911593e8a084653513136fc2310f4841c111b4
Aigul <sharipova.aig@gmail.com>
2022-06-14 22:39:38 -0400

    function

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const btn = document.querySelector('.btn');
 const toDoContainer = document.querySelector('.toDoContainer');
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () => {
 const item = document.createElement('li');
 item.innerText = inputField.value;
 item.classList.add('toDoAdded');
 toDoContainer.appendChild(item);
 inputField.value = '';
 
-item.addEventListener('click', function(){
+item.addEventListener('click', () => {
   item.classList.add('toDoCompleted')
 })
 
-  item.addEventListener('dblclick', function() {
+  item.addEventListener('dblclick', () => {
     toDoContainer.removeChild(item);
   })
 })
